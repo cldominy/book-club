e420538f0a8eb2c04423a399577859bbcf677ef4
Chris Dominy <cdominy@utexas.edu>
2020-10-26 19:38:16 -0500

    Added username field to displaying purposes

--- models/user.js
+++ models/user.js
@@ -12,6 +12,11 @@ module.exports = (sequelize, DataTypes) => {
                 isEmail: true
             }
         },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
         // The password cannot be null
         password: {
             type: DataTypes.STRING,
