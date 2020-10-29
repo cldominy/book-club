77e762c7af14db2d3d33bda7152afc330908a8fd
amandaneal1131 <amandaneal1131@gmail.com>
2020-10-28 21:09:41 -0500

    created and added js file

--- public/js/review.js
+++ public/js/review.js
@@ -5,18 +5,27 @@ $(document).ready(() => {
     const AuthorLastName = $("#author-last");
     const bookReview = $("#book-review");
 
-    bookForm.on("submit", event => {
+    bookForm.on(submit, (event) => {
         event.preventDefault();
         const reviewData = {
             title: bookTitle.val(),
             firstname: AuthorFirstName.val(),
             lastname: AuthorLastName.val(),
-            review: bookReview.val()
+            review: bookReview.val(),
         };
-        if (!reviewData.title || !reviewData.firstname || !reviewData.lastname || !reviewData.review) {
-            return
+        if (!reviewData.title ||
+            !reviewData.firstname ||
+            !reviewData.lastname ||
+            !reviewData.review
+        ) {
+            return;
         }
-        logReview(reviewData.title, reviewData.firstname, reviewData.lastname, reviewData.review);
+        logReview(
+            reviewData.title,
+            reviewData.firstname,
+            reviewData.lastname,
+            reviewData.review
+        );
         bookTitle.val("");
         AuthorFirstName.val("");
         AuthorLastName.val("");
@@ -25,18 +34,25 @@ $(document).ready(() => {
 
     function logReview(title, firstname, lastname, review) {
         $.post("/api/reviews", {
-            title: title,
-            firstname: firstname,
-            lastname: lastname,
-            review: review
-        }).then(() => {
-            window.location.replace("/display");
-        }).catch(handleReviewErrors)
+            title:title,
+            firstname:firstname,
+            lastname:lastname,
+            review:review,
+        })
+            .then(() => {
+                window.location.replace("/display");
+            })
+            .catch(handleReviewErrors);
     }
 
     function handleReviewErrors(err) {
         let message;
-        if (err && err.responseJSON && err.responseJSON.errors && err.responseJSON.errors[0]) {
+        if (
+            err &&
+            err.responseJSON &&
+            err.responseJSON.errors &&
+            err.responseJSON.errors[0]
+        ) {
             message = err.responseJSON.errors[0].message;
         } else {
             message = "An unknown error occurred; please try again later";
@@ -45,5 +61,4 @@ $(document).ready(() => {
         $("#alert .msg").text(message);
         $("#alert").fadeIn(500);
     }
-
 });
