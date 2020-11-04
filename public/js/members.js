bf247c17bdc63881acb8bd66fb97049844f42de4
HallRB <Richardbrianhall@gmail.com>
2020-11-04 15:36:53 -0600

    removed js errors

--- public/js/members.js
+++ public/js/members.js
@@ -9,35 +9,3 @@ $(document).ready(() => {
 });
 
 
-showSlides(slideIndex);
-        
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-        
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-        
-function showSlides(n) {
-    let i;
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1;}    
-    if (n < 1) {slideIndex = slides.length;}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-}
-
-const textarea = document.getElementById("review");
-const heightLimit = 200; /* Maximum height: 200px */
-textarea.oninput = function() {
-    textarea.style.height = ""; /* Reset the height*/
-    textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
-};
